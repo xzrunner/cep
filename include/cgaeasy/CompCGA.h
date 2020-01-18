e1652ddc85872352ba4b9e310c038ceb7ffd7179
zz <xzrunner@gmail.com>
2020-01-18 17:31:37 +0800

    [ADDED] rule's eval_ctx

--- include/cgaeasy/CompCGA.h
+++ include/cgaeasy/CompCGA.h
@@ -4,7 +4,7 @@
 
 #include <string>
 
-namespace cga { class EvalRule; }
+namespace cga { class EvalRule; class EvalContext; }
 
 namespace cgae
 {
@@ -20,15 +20,18 @@ public:
     void SetFilepath(const std::string& filepath) { m_filepath = filepath; }
     const std::string& GetFilepath() const { return m_filepath; }
 
-    void  SetRule(const std::shared_ptr<cga::EvalRule>& rule);
+    void  SetRule(const std::shared_ptr<cga::EvalRule>& rule,
+        const std::shared_ptr<cga::EvalContext>& rule_ctx);
     auto& GetRule() const { return m_rule; }
+    auto& GetRuleCtx() const { return m_rule_ctx; }
 
     static constexpr char* const TYPE_NAME = "comp_cga";
 
 private:
     std::string m_filepath;
 
-    std::shared_ptr<cga::EvalRule> m_rule = nullptr;
+    std::shared_ptr<cga::EvalContext> m_rule_ctx = nullptr;
+    std::shared_ptr<cga::EvalRule>    m_rule     = nullptr;
 
 }; // CompCGA
 
--- source/CompCGA.cpp
+++ source/CompCGA.cpp
@@ -6,13 +6,15 @@ namespace cgae
 std::unique_ptr<n0::NodeComp> CompCGA::Clone(const n0::SceneNode& node) const
 {
     auto comp = std::make_unique<CompCGA>();
-    comp->SetRule(m_rule);
+    comp->SetRule(m_rule, m_rule_ctx);
     return comp;
 }
 
-void CompCGA::SetRule(const std::shared_ptr<cga::EvalRule>& rule)
+void CompCGA::SetRule(const std::shared_ptr<cga::EvalRule>& rule,
+                      const std::shared_ptr<cga::EvalContext>& rule_ctx)
 {
     m_rule = rule;
+    m_rule_ctx = rule_ctx;
 }
 
 }
